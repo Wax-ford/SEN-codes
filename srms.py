635b889f1787ed0b8a80925331c3f49b9fe2544f
WaxFord <wealthadediran@gmail.com>
2025-12-30 23:52:29 +0100

    bug fix

--- srms.py
+++ srms.py
@@ -7,15 +7,15 @@ test_score = float(input("Enter test score: "))
 exam_score = float(input("Enter exam score: "))
 
 total_score = test_score + exam_score
-#average_score = total_score / 2
+average_score = total_score / 2
 
-if total_score >= 70:
+if average_score >= 70:
     grade = 'A'
-elif total_score >= 60:
+elif average_score >= 60:
     grade = 'B'
-elif total_score >= 50:
+elif average_score >= 50:
     grade = 'C'
-elif total_score >= 45:
+elif average_score >= 45:
     grade = 'D'
 else: grade = 'F'
 
@@ -23,5 +23,5 @@ print("\n----Student Result----")
 print("Name:", student_name)
 print("Matric Number:", matric_number)
 print("Total Score:", total_score)
-# print("Average Score:", average_score)
+print("Average Score:", average_score)
 print("Grade:", grade)
